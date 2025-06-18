b2d1e4ab6e3c0205f7da9dfc157e8a07157a0011
Palmer Dabbelt <palmer@dabbelt.com>
2025-06-18 12:33:51 -0700

    Make that futex test compile
    Signed-off-by: Palmer Dabbelt <palmer@dabbelt.com>

--- rtm-futex.c
+++ rtm-futex.c
@@ -1,5 +1,16 @@
 #include <linux/futex.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <time.h>
 
-main(){
+long futex(uint32_t *uaddr, int futex_op, uint32_t val,
+           const struct timespec *timeout,
+           uint32_t *uaddr2, uint32_t val3) {
+    return syscall(SYS_futex, uaddr, futex_op, val, timeout, uaddr2, val3);
+}
+
+int main(){
   futex((void*) 0x1ffffff81300000, FUTEX_WAIT, 0, 0, 0, 0);
+  return 0;
 }
